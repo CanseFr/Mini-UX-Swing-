7a73d2f797ac109f5ea7a4ccd90910774699ef88
Canse <49596442+CanseFr@users.noreply.github.com>
2023-04-25 19:11:45 +0200

    Update Triangle.java

--- Constructing a triangle/Triangle/src/triangle/Triangle.java
+++ Constructing a triangle/Triangle/src/triangle/Triangle.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package triangle;
 
-/**
- *
- * @author Bluelime Learning So
- */
+
 import java.io.*;
 public class Triangle {
 
