afa6c313ff31b43c76f8ee54f9909a884fc85060
Canse <49596442+CanseFr@users.noreply.github.com>
2023-04-25 19:14:23 +0200

    Update bmi.java

--- Create BMI Tool/BMI TooL/src/BMI/Tool/bmi.java
+++ Create BMI Tool/BMI TooL/src/BMI/Tool/bmi.java
@@ -1,20 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BMI.Tool;
 
 import java.text.DecimalFormat;
 
-/**
- *
- * @author Bluelime Learning So
- */
 public class bmi extends javax.swing.JFrame {
 
     /**
-     * Creates new form bmi
+     * Creates new form bmi ( chercher formule bmi )
      */
     public bmi() {
         initComponents();
