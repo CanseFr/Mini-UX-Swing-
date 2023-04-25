262eb002efe4b1a5b48ec2f25a1ae8ba48542de7
Canse <49596442+CanseFr@users.noreply.github.com>
2023-04-25 19:50:05 +0200

    Update PrimeNumbers.java

--- Generate prime numbers/PrimeNumbers/src/primenumbers/PrimeNumbers.java
+++ Generate prime numbers/PrimeNumbers/src/primenumbers/PrimeNumbers.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package primenumbers;
 
-/**
- *
- * @author Bluelime Learning So
- */
+ 
 import java.io.*;
 
 public class PrimeNumbers {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         int i;
